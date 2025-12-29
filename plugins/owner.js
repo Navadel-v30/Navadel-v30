9195eade16e117c06db744f5e02d9db12034ca2c
Navadel-v30 <acepruli111@gmail.com>
2025-12-30 01:45:13 +0700

    update

--- plugins/owner.js
+++ plugins/owner.js
@@ -1,13 +1,19 @@
 import config from "../config.js"
 
 export default {
-  command: ["restart"],
-  run: async ({ sock, msg }) => {
-    const sender = msg.key.participant.split("@")[0]
-    if (!config.owner.includes(sender))
-      return sock.sendMessage(msg.key.remoteJid,{ text:"❌ Owner only"})
+  command: ["restart","eval"],
+  category: "Owner",
+  desc: "Owner tools",
 
-    await sock.sendMessage(msg.key.remoteJid,{ text:"♻️ Restart..."})
-    process.exit()
+  run: async ({ sock, msg, args }) => {
+    const sender = msg.key.participant.split("@")[0]
+    if (!config.owner.includes(sender)) {
+      return sock.sendMessage(msg.key.remoteJid,{ text:"❌ Owner only" })
+    }
+
+    if (args[0] === "restart") {
+      await sock.sendMessage(msg.key.remoteJid,{ text:"♻️ Restarting..." })
+      process.exit()
+    }
   }
 }
