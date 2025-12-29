5fdefa00e32835990e779b7e13f16972750a2400
Navadel-v30 <acepruli111@gmail.com>
2025-12-30 01:28:37 +0700

    update

--- plugins/game-suit.js
+++ plugins/game-suit.js
@@ -2,10 +2,10 @@ const suit = ["batu","gunting","kertas"]
 
 export default {
   command: ["suit"],
-  run: async ({ sock, msg }) => {
-    const user = msg.message.conversation.split(" ")[1]
+  run: async ({ sock, msg, args }) => {
+    const user = args[0]
     if (!suit.includes(user))
-      return sock.sendMessage(msg.key.remoteJid,{ text:"Pilih: batu / gunting / kertas"})
+      return sock.sendMessage(msg.key.remoteJid,{ text:"batu / gunting / kertas"})
 
     const bot = suit[Math.floor(Math.random()*3)]
     sock.sendMessage(msg.key.remoteJid,{
